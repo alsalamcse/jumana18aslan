f95addf85af06b95cc46f08c1136c58daa707968
Ayah <aslanayah3@gmail.com>
2019-09-19 14:07:24 +0300

    eswtgdft

--- app/src/main/java/com/asslan/jumana/jumanataskmanger/wellcome.java
+++ app/src/main/java/com/asslan/jumana/jumanataskmanger/wellcome.java
@@ -1,13 +1,31 @@
 package com.asslan.jumana.jumanataskmanger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class wellcome extends AppCompatActivity {
+public class wellcome extends AppCompatActivity
+{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellcome);
+
+        Thread th=new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(3*1000);
+                    Intent i=new Intent(getApplicationContext(),signIn.class);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        th.start();
     }
 }
