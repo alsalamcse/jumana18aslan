6f7b1a22e2a1069f4dcfade2f3368f2020dd9cc5
Ayah <aslanayah3@gmail.com>
2019-09-23 09:42:16 +0300

    eswtgdft

--- app/src/main/java/com/asslan/jumana/jumanataskmanger/signIn.java
+++ app/src/main/java/com/asslan/jumana/jumanataskmanger/signIn.java
@@ -2,8 +2,11 @@ package com.asslan.jumana.jumanataskmanger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signIn extends AppCompatActivity {
     private EditText emsign,pssign;
@@ -21,8 +24,68 @@ public class signIn extends AppCompatActivity {
         signUp1=findViewById(R.id.signUp1);
 
 
+        singIn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataHandler();
+            }
+
+            private void dataHandler() {
+                String email=emsign.getText().toString();
+                String password=pssign.getText().toString();
+                boolean isok=true;
+//                if(email.length()<4)
+//                {
+//                    emsign.setError("Email length eror");
+//                   isok=false;
+//                }
+//                if(email.indexOf("@")<0 || email.indexOf(".")<0)
+//                {
+//                    emsign.setError("email wrong format");
+//                    isok=false;
+//                }
+                if(isValidEmailAddress(email)==false)
+                {
+                    emsign.setError("Invalid Emaill");
+                    isok=true;
+                }
+                if(password.length()<8)
+                {
+                    pssign.setError("password length error");
+                    isok=false;
+                }
+
+                if(isok)
+                {
 
 
+                }
+
+
+
+
+
+                //singIn(email,passw)
+
+            }
+        });
+
+
+
+
+    }
+    public boolean isValidEmailAddress(String email)
+    {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    private void signIn(String email, String passw)
+    {
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email.passw);
     }
 
 }
