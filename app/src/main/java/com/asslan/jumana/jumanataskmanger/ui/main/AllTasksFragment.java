0b75b4707f46eb26e00a87a6095abb966baee67c
Ayah <aslanayah3@gmail.com>
2019-11-10 10:36:24 +0200

    eswtgdft

--- app/src/main/java/com/asslan/jumana/jumanataskmanger/ui/main/AllTasksFragment.java
+++ app/src/main/java/com/asslan/jumana/jumanataskmanger/ui/main/AllTasksFragment.java
@@ -8,9 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.asslan.jumana.jumanataskmanger.R;
 import com.asslan.jumana.jumanataskmanger.data1.Task;
+import com.asslan.jumana.jumanataskmanger.data1.TaskAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,6 +24,8 @@ import com.google.firebase.database.ValueEventListener;
  * A simple {@link Fragment} subclass.
  */
 public class AllTasksFragment extends Fragment {
+    private TaskAdapter taskAdapter;
+    private ListView lvTasks;
 
 
     public AllTasksFragment() {
@@ -32,8 +36,18 @@ public class AllTasksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        taskAdapter=new TaskAdapter(getContext());
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_tasks, container, false);
+        View view=inflater.inflate(R.layout.fragment_all_tasks,container,false);
+        lvTasks=view.findViewById(R.id.lstvTasks);
+        lvTasks.setAdapter(taskAdapter);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        readTasksFromFirebase();
     }
 
     public void readTasksFromFirebase()
@@ -47,10 +61,12 @@ public class AllTasksFragment extends Fragment {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) 
             {
+                taskAdapter.clear();
                 for (DataSnapshot d:dataSnapshot.getChildren())
                 {
-                    Task t=d.getValue(Task.class);
-                    Log.d("Task",t.toString());
+                    Task task=d.getValue(Task.class);
+                    Log.d("Task",task.toString());
+                    taskAdapter.add(task);
                 }
                 
             }
