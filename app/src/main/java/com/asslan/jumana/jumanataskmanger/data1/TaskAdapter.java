fe8b6866cd39648535c4a2972641a595aa5e5715
Ayah <aslanayah3@gmail.com>
2019-11-19 10:50:03 +0200

    eswtgdft

--- app/src/main/java/com/asslan/jumana/jumanataskmanger/data1/TaskAdapter.java
+++ app/src/main/java/com/asslan/jumana/jumanataskmanger/data1/TaskAdapter.java
@@ -1,6 +1,8 @@
 package com.asslan.jumana.jumanataskmanger.data1;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,7 +12,6 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.QuickContactBadge;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,7 +19,6 @@ import android.widget.Toast;
 import com.asslan.jumana.jumanataskmanger.R;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class TaskAdapter extends ArrayAdapter<Task>
 {
@@ -34,7 +34,7 @@ public class TaskAdapter extends ArrayAdapter<Task>
         TextView tvSubject=vitem.findViewById(R.id.itmTvSubject);
         RatingBar rbPrio=vitem.findViewById(R.id.itmRatingPrio);
         CheckBox cbIsCompleted=vitem.findViewById(R.id.itmChbxIsCompleted);
-        ImageView itmImgInfo=vitem.findViewById(R.id.itmImgInfo);
+        ImageView itmImgInfo1=vitem.findViewById(R.id.itmImgInfo1);
 
 
 
@@ -65,7 +65,7 @@ public class TaskAdapter extends ArrayAdapter<Task>
             }
         });
 
-        itmImgInfo.setOnClickListener(new View.OnClickListener() {
+        itmImgInfo1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext(),myTask.getTitle(), Toast.LENGTH_SHORT).show();
@@ -83,7 +83,22 @@ public class TaskAdapter extends ArrayAdapter<Task>
 
         return vitem;
     }
-    public void ShowMenu(){
+    public void ShowMenu()
+    {
+        final String[] option={"Add" ,"View" , "Select" , "Delete"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.select_dialog_item,option);
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Select option");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+            }
+        });
+        final AlertDialog a=builder.create();
+        a.show();
+
 
     }
 }
